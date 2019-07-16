ece7c80b9f5ec427df3f1db2462bf0be066df0e8
uchikun2493 <um2493@gmail.com>
2019-07-16 11:34:02 +0900

    update

--- src/sample.py
+++ src/sample.py
@@ -1,5 +1,5 @@
 
-import sys
+import os, sys
 import numpy as np
 import matplotlib.pyplot as plt
 # Chainer
@@ -19,22 +19,19 @@ if __name__ == '__main__':
     if 2 <= len(args):
         if args[1] == '-1':
             train_mode = False
-
-    # 学習する手書き数字の数値
-    number = 5
-
-    # 学習の条件
-    # エポックとミニバッチサイズ
-    epoch = 200
-    batchsize = 32
-    # 隠れ層のユニット数
-    hidden = 100
+    
+    # 出力先のフォルダを生成
+    save_dir = '../output/'
+    os.makedirs(save_dir, exist_ok = True)
+    # 作成するモデルの保存先+名前
+    save_model_name = os.path.join(save_dir, 'mymodel.npz')
 
     # MNISTデータの読み込み
     train, test = chainer.datasets.get_mnist()
     
     # データとラベルに切り分け(ラベルは不要)
-    # 指定したラベルで抜き取り
+    # 指定したnumber(ラベル)で抜き取り
+    number = 5
     train_data, train_label = train._datasets
     train_data = train_data[train_label == number]
 
@@ -44,16 +41,23 @@ if __name__ == '__main__':
     test_data  = np.concatenate((test_data_n[0:5],  test_data[0:5]))
     test_label = np.concatenate((test_label_n[0:5], test_label[0:5]))
 
+    # 学習の条件
+    # エポックとミニバッチサイズ
+    epoch = 200
+    batchsize = 32
+    # 隠れ層のユニット数
+    hidden = 100
+
     # AutoEncoderの学習
     # コマンドライン引数が'-1'の場合学習しない
     if train_mode:
         # 学習＋モデルの保存
         model = train_ae(train_data, train_data.shape[1], hidden, epoch, batchsize)
-        serializers.save_npz("../output/mymodel.npz", model)
+        serializers.save_npz(save_model_name, model)
 
     # 保存したモデルから読み込み
     model = Autoencoder(784, hidden)
-    serializers.load_npz("../output/mymodel.npz", model)
+    serializers.load_npz(save_model_name, model)
 
     # 再構成を行う
     # AutoEncoderの再構成を行うクラスを定義
@@ -71,13 +75,13 @@ if __name__ == '__main__':
     # plt.title('dat2')
     # plt.show()
 
-    mn, std, th = ar.err2threshold(train_err, 2)
+    mn, std, th = ar.err2threshold(err_train, 2)
     print("th: ", th)
 
     #print(labels[0:9])
     print("label: ",test_label)
 
-    result = ar.judgment(test_err, th)
+    result = ar.judgment(err_test, th)
     print("result: ", result)
-    print("errors: ", test_err)
+    print("errors: ", err_test)
 
