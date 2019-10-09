99481a84241ac590aa3ab504357b3545c182721f
YanoSanso <x.5ds.o.6ds@gmail.com>
2019-10-09 11:05:39 +0900

    using layer name

--- src/variational_autoencoder.py
+++ src/variational_autoencoder.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import chainer
 import chainer.functions as F
@@ -6,18 +5,11 @@ import chainer.links as L
 import chainer.computational_graph as c
 from chainer import Variable, optimizers, initializer,cuda
 from chainer.functions.loss.vae import gaussian_kl_divergence
-
 import cupy
-
 import os
 
+# Xavier initializaer : used in class VAE
 class Xavier(initializer.Initializer):
-    """
-    Xavier initializaer 
-    Reference: 
-    * https://jmetzen.github.io/2015-11-27/vae.html
-    * https://stackoverflow.com/questions/33640581/how-to-do-xavier-initialization-on-tensorflow
-    """
     def __init__(self, fan_in, fan_out, constant=1, dtype=None):
         self.fan_in = fan_in
         self.fan_out = fan_out
@@ -36,88 +28,81 @@ class Xavier(initializer.Initializer):
         array[...] = xp.random.uniform(**args)
 
 
+#Variational AutoEncoder model
 class VAE(chainer.Chain):
-    """Variational AutoEncoder"""
-
     def __init__(self, layers, act_func=F.tanh):
         super(VAE, self).__init__()
         self.act_func = act_func
-        self.prop_num = len(layers)-1
-
         self.makeLayers(layers)
 
-
     def makeLayers(self,hidden):
-        
-        for i in range(len(hidden)-1):
+        # レイヤー作成. 作成順は任意だが,children()を使う関係上 nameが前伝搬の順に昇順でなければならない.
+        # そのため、encoderは入力層に近いほど数字が若く、decoderはボトルネックに近いほど数字を若くする必要がある.
+        for i in range(len(hidden)-2):
+            # エンコード層を入力層からボトルネックの順で作成
             l = L.Linear(hidden[i],hidden[i+1], initialW=Xavier(hidden[i], hidden[i+1]))
-            name  = 'a_enc{}'.format(i)
+            name  = 'enc{}'.format(i)
             self.add_link(name,l)
-
+            # デコード層をボトルネックから出力層の順で作成
             j = len(hidden)-i-1
             l = L.Linear(hidden[j],hidden[j-1], initialW=Xavier(hidden[i], hidden[i+1]))
-            name  = 'b_dec{}'.format(i)
+            name  = 'dec{}'.format(i)
             self.add_link(name,l)
 
-        l = L.Linear(hidden[i],hidden[i+1], initialW=Xavier(hidden[i], hidden[i+1]))
-        name  = 'a_enc{}_var'.format(i)
-        self.add_link(name,l)
+        # μ,σを出力する層はここで定義
+        self.add_link('enc_mu' ,L.Linear(hidden[-2],hidden[-1], initialW=Xavier(hidden[-2], hidden[-1])))
+        self.add_link('enc_var',L.Linear(hidden[-2],hidden[-1], initialW=Xavier(hidden[-2], hidden[-1])))
+        # 出力層はここで定義
+        self.add_link('dec_out',L.Linear(hidden[1],hidden[0],   initialW=Xavier(hidden[1] , hidden[0] )))
 
         
     def __call__(self, x, sigmoid=True):
-        """ AutoEncoder """
-        e,var = self.encode(x)
+        mu,var = self.encode(x)
+        e = F.gaussian(mu, var)
         d = self.decode(e,sigmoid)
         return e,d
 
-
-    def encode(self, x):
-        
+    def encode(self, x):        
         e = x
-
         for i,layer in enumerate(self.children()):
-            # print(layer.name)
-            if i == self.prop_num-1:
-                mu = layer(e)
-                continue
-            if i == self.prop_num:
-                var = layer(e)
-                break
-            e = self.act_func(layer(e))
+            # エンコード層のみ処理
+            if 'enc' in layer.name:
+                if 'mu' in layer.name: # μ を出力するレイヤー
+                    mu = layer(e)
+                    continue
+                if 'var' in layer.name: # σ を出力するレイヤー
+                    var = layer(e)
+                    break
+                # 中間層の順伝搬と活性化
+                e = self.act_func(layer(e))
 
         return mu,var
 
     def decode(self, z, sigmoid=True):
-
-        # sigmoid = True
         d = z
-
         for i,layer in enumerate(self.children()):
-            if i > self.prop_num:
-                d = layer(d)
-                if i < self.prop_num * 2:
-                    d = self.act_func(d)
-                    # print(layer.name)
+            # デコード層のみ処理
+            if 'dec' in layer.name:
+                # 出力層は活性化しない
+                if 'out' in layer.name: 
+                    d = layer(d)
+                    break
+                d = self.act_func(layer(d))
 
         if sigmoid:
             d = F.sigmoid(d)
-
         return d
-
-
 
 
 # 再構成と再構成誤差の計算
 class Reconst():
     # 学習、モデルを渡しておく
-    def __init__(self, model,sig=False):
-        
+    def __init__(self, model,sig=True):
         self.model = model
         self.sig = sig
     
     # 再構成と再構成誤差一括で計算
     def __call__(self, data):
-
         # 配列の次元数を調べる(1次元だとエラーを出すため)
         if data.ndim == 1:
             data = data.reshape(1, len(data))
@@ -128,23 +113,12 @@ class Reconst():
 
     # 入力データを再構成
     def data2reconst(self, data):
-        feat = Variable(data)
-        mu,var = self.model.encode(feat)
-        feat = F.gaussian(mu, var)
-        
-        reconst = feat
-        reconst = self.model.decode(reconst,sigmoid=self.sig)
-
+        feat, reconst = self.model(Variable(data),self.sig)
         return feat.data, reconst.data
 
     def decode(self,data):
-        reconst = Variable(data)
-        reconst = self.model.decode(reconst,sigmoid=self.sig)
-        # reconst = self.model.decode(reconst,sigmoid=True)
-
+        reconst = self.model.decode(Variable(data),self.sig)
         return reconst
-
-
 
     # 再構成誤差の計算
     def reconst_err(self, data, reconst):
@@ -163,7 +137,7 @@ class Reconst():
         result = [True if err[i] <= th else False for i in range(len(err))]
         return result
 
-
+# 実際に学習する部分のコード
 def train_vae(model,train,epoch,batch,C=1.0,k=1,use_sigmoid=False):
 
     model.to_gpu(0)
@@ -178,23 +152,21 @@ def train_vae(model,train,epoch,batch,C=1.0,k=1,use_sigmoid=False):
     for ep in range(epoch):
         perm = np.random.permutation(nlen)
         for p in range(0,nlen,batch):
-            d = data[perm[p:p+batch]]
 
-            # エンコード
-            enc = d
+            input_data = data[perm[p:p+batch]]
             model.cleargrads()
-            mu, ln_var = model.encode(enc)
+            # エンコード
+            mu, ln_var = model.encode(input_data)
 
             rec_loss = 0
-            for l in range(k): #普通は k=1
-                z = F.gaussian(mu, ln_var)
+            for l in range(k): # 基本的にはk=1
+                dec = F.gaussian(mu, ln_var)
 
-                # デコード
-                dec = z
-                dec = model.decode(dec, sigmoid=use_sigmoid)
+                # デコード, bernoulli_nllがsigmoidを内包しているので,学習時は sigmoid = False
+                reconst_data = model.decode(dec, sigmoid=False)
 
-                rec_loss += F.bernoulli_nll(d,dec) / (k * batch)
-                # rec_loss += F.mean_squared_error(d,dec) / (k)
+                rec_loss += F.bernoulli_nll(input_data,reconst_data) / (k * batch)
+                # rec_loss += F.mean_squared_error(input_data,reconst_data) / (k)
 
             latent_loss = C * gaussian_kl_divergence(mu, ln_var) / batch
             loss = rec_loss + latent_loss
@@ -211,7 +183,6 @@ def train_vae(model,train,epoch,batch,C=1.0,k=1,use_sigmoid=False):
     print()
 
     model.to_cpu()
-
     return model
 
 def train_stacked(train, hidden, epoch, batchsize, folder, \
@@ -222,7 +193,6 @@ def train_stacked(train, hidden, epoch, batchsize, folder, \
     if type(hidden) != type([]):
         hidden = [hidden]
     layer  = [inputs] + hidden
-    # layer.extend(hidden)
     
     # 隠れ層の数値を文字列にして保存・読み込みのフォルダを分ける
     hidden_str = []
@@ -235,22 +205,12 @@ def train_stacked(train, hidden, epoch, batchsize, folder, \
     folder_model = os.path.join(folder, hidden_num_str)
     os.makedirs(folder_model, exist_ok=True)
 
-
-    # 隠れ層分だけloop
-    model = []
-    feat = train.copy()
-        
-    # 保存に使う文字列
-    hidden_num = ''
-    for i in layer:
-        hidden_num += str(i) + '_'
     # モデルの保存名
-    save_name = os.path.join(folder_model, 'model_' + hidden_num + '.npz')
+    save_name = os.path.join(folder_model, 'model.npz')
     if train_mode:
         model = VAE(layer, act)
         model = train_vae(model, train, epoch, batchsize,C=C,k=k,use_sigmoid=use_sigmoid)
         chainer.serializers.save_npz(save_name, model)
-
     # 学習しない場合はloadする
     else:
         model = VAE(layer, act)
