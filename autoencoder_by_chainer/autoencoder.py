e295055efdd2b6f15ea8b759499e5e57fc9f8bf3
uchikun2493 <um2493@gmail.com>
2019-12-13 11:05:33 +0900

    fix: creating pdf file

--- autoencoder_by_chainer/autoencoder.py
+++ autoencoder_by_chainer/autoencoder.py
@@ -3,6 +3,8 @@ import os
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
 import chainer
 import chainer.functions as F
 import chainer.links as L
